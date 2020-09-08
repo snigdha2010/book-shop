70d388bd6771431cb4ae7ac3dc8e3f5b61675a75
snighdha2010 <snigdha.sahara@gmail.com>
2020-09-08 13:17:51 -0400

    assignmennt done

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Books.css';
 
 const Books = (props) => {
-    const {name,price} = props.productDetail;
+    const {name,price,img} = props.productDetail;
+    console.log(img)
     const getPrice = props.handleProduct;
     return (
                 <div className = 'col-md-3 p-4 m-4 w-50 book-container'>
+                <img src={img} alt=""/>
                 <h3>{name}</h3>
                 <p>Price: {price}</p>
                 <button onClick={()=>getPrice(price)} className='btn btn-primary'>Buy Now</button>
